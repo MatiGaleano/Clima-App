36827a130d9c2d04d1600333076c71c24df154b8
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-06-10 21:51:24 -0300

    componente card

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { WeatherData } from 'src/app/models/weather.interface';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent {
+
+  @Input() public weather!: WeatherData;
+  public BASE_URL = 'http://openweathermap.org/img/wn'
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
